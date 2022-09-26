052ac6749c2d39210872741ad1f0f37b60d0da44
Santiago <gosanti988@gmail.com>
2022-09-26 00:58:10 -0500

    Primera clase de javascript para curso de react

---
+++ src/bases/01-const-let.js
@@ -0,0 +1,13 @@
+const nombre= 'Santiago' 
+let apellido = 'Cuesta'
+
+let valorDado = 5;
+valorDado = 4;
+console.log(nombre, apellido, valorDado);
+
+if (true) {
+    const nombre ='Peter';
+    console.log(nombre);
+}
+
+console.log(nombre);
