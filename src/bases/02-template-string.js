c966c69d4250ac528d1cd4a2c412e8dd1532bfe4
Santiago <gosanti988@gmail.com>
2022-09-26 01:06:24 -0500

    clase de unos de apostrofes para escrobir codigo javascript

---
+++ src/bases/02-template-string.js
@@ -0,0 +1,9 @@
+const nombre = "Santiago";
+const apellido = "Cuesta";
+
+const nombreCompleto= `${nombre} ${apellido}`;
+console.log(nombreCompleto); 
+function getSaludo(){
+    return 'Hola mundo';
+}
+console.log(`Este es un texto: ${getSaludo()}`);
