cf74bac60fcb056232d44675186644ccba3fdf11
Santiago <gosanti988@gmail.com>
2022-09-29 11:01:44 -0500

    desestructuracion de arrays

---
+++ src/bases/desestructuracion-arrays.js
@@ -0,0 +1,25 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+//Desestructuracion de arreglos
+
+const personajes =['Goku', 'Vegueta', 'Trunks'];
+const [,,p3]=personajes;
+console.log(p3);
+
+const retornarArreglo = () => {
+    return ['ABC' ,123];
+}
+
+const[letras, numeros] = retornarArreglo();
+
+console.log(letras, numeros);
+
+//1. el primer valor del arr se llamará nombre
+//2. se llamará setNombre
+const useState = (valor)=> {
+    return[valor, ()=>{console.log('hola mundo')}]
+}
+const[nombre, setNombre]= useState('Goku');
+
+console.log(nombre)
+setNombre();
+
