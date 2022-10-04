5054ec4e7cbe00ff807a73a0a9d5cb91d3257b84
Santiago <gosanti988@gmail.com>
2022-10-04 17:42:21 -0500

    operadores ternarios

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// operadores ternarios 
+const activo = true;
+// let mensaje ='';
+// if (!activo) {
+//     mensaje ='Activo';
+// }else{
+// mensaje = 'Inactivo'
+// }
+
+// const mensaje = (!activo) ? 'Activo': 'Inactivo';
+const mensaje = (activo) && 'Activo' ;
+console.log(mensaje);
